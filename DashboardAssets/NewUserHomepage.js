d18771041dd6cdee6326e534a49b6cc48dbbecb2
SteffQing <steveola23@gmail.com>
2023-04-27 11:31:36 +0100

    Added Dashboard assets

---
+++ DashboardAssets/NewUserHomepage.js
@@ -0,0 +1,327 @@
+import * as React from "react";
+const AddPaymentMethodIcon = (props) => (
+  <svg
+    width={76}
+    height={76}
+    viewBox="0 0 76 76"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M38.0007 69.6673C55.4173 69.6673 69.6673 55.4173 69.6673 38.0007C69.6673 20.584 55.4173 6.33398 38.0007 6.33398C20.584 6.33398 6.33398 20.584 6.33398 38.0007C6.33398 55.4173 20.584 69.6673 38.0007 69.6673Z"
+      stroke="#4D6B8C"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M25.334 38H50.6673"
+      stroke="#4D6B8C"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M38 50.6673V25.334"
+      stroke="#4D6B8C"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const CalendarIcon = (props) => (
+  <svg
+    width={40}
+    height={40}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M13.334 3.33398V8.33398"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M26.666 3.33398V8.33398"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5.83398 15.1504H34.1673"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M35 14.1673V28.334C35 33.334 32.5 36.6673 26.6667 36.6673H13.3333C7.5 36.6673 5 33.334 5 28.334V14.1673C5 9.16732 7.5 5.83398 13.3333 5.83398H26.6667C32.5 5.83398 35 9.16732 35 14.1673Z"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M26.1582 22.8333H26.1731"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M26.1582 27.8333H26.1731"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19.9921 22.8333H20.0071"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19.9921 27.8333H20.0071"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.8242 22.8333H13.8391"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M13.8242 27.8333H13.8391"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const CardPOSIcon = (props) => (
+  <svg
+    width={40}
+    height={40}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M6.54883 26.4655L26.4655 6.54883"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M18.502 30.4648L20.502 28.4648"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M22.9883 25.9814L26.9716 21.998"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6.00277 17.0652L17.0694 5.9985C20.6028 2.46516 22.3694 2.4485 25.8694 5.9485L34.0528 14.1318C37.5528 17.6318 37.5361 19.3985 34.0028 22.9318L22.9361 33.9985C19.4028 37.5318 17.6361 37.5485 14.1361 34.0485L5.95277 25.8652C2.45277 22.3652 2.45277 20.6152 6.00277 17.0652Z"
+      stroke="#656568"
+      strokeWidth={2.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M3.33398 36.6641H36.6673"
+      stroke="#656568"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const AwardIcon = (props) => (
+  <svg
+    width={40}
+    height={40}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M36.101 17.4008L34.4676 24.3675C33.0676 30.3842 30.301 32.8175 25.101 32.3175C24.2676 32.2508 23.3676 32.1008 22.401 31.8675L19.601 31.2008C12.651 29.5508 10.501 26.1175 12.1343 19.1508L13.7676 12.1675C14.101 10.7508 14.501 9.51749 15.001 8.50083C16.951 4.46749 20.2676 3.38416 25.8343 4.70083L28.6176 5.35083C35.601 6.98416 37.7343 10.4342 36.101 17.4008Z"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M25.1006 32.3167C24.0672 33.0167 22.7672 33.6 21.1839 34.1167L18.5506 34.9833C11.9339 37.1167 8.45057 35.3333 6.30057 28.7167L4.16724 22.1333C2.0339 15.5167 3.80057 12.0167 10.4172 9.88333L13.0506 9.01667C13.7339 8.8 14.3839 8.61667 15.0006 8.5C14.5006 9.51667 14.1006 10.75 13.7672 12.1667L12.1339 19.15C10.5006 26.1167 12.6506 29.55 19.6006 31.2L22.4006 31.8667C23.3672 32.1 24.2672 32.25 25.1006 32.3167Z"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M21.0664 14.2168L29.1497 16.2668"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M19.4336 20.666L24.2669 21.8994"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const StarIcon = (props) => (
+  <svg
+    width={14}
+    height={14}
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M5.97118 1.37448C6.43757 0.599498 7.56113 0.599497 8.02752 1.37448L9.15105 3.2414C9.3186 3.51981 9.5919 3.71838 9.90846 3.79169L12.0312 4.28332C12.9124 4.4874 13.2596 5.55597 12.6666 6.23902L11.2383 7.88446C11.0253 8.12985 10.9209 8.45113 10.949 8.77486L11.1374 10.9456C11.2156 11.8467 10.3066 12.5071 9.47376 12.1543L7.46746 11.3043C7.16826 11.1776 6.83044 11.1776 6.53124 11.3043L4.52494 12.1543C3.6921 12.5071 2.78312 11.8467 2.86133 10.9456L3.04972 8.77486C3.07782 8.45113 2.97343 8.12985 2.76041 7.88446L1.33206 6.23902C0.73913 5.55597 1.08633 4.4874 1.9675 4.28332L4.09024 3.79169C4.4068 3.71838 4.6801 3.51981 4.84765 3.2414L5.97118 1.37448Z"
+      stroke="#939697"
+      strokeWidth={1.5}
+    />
+  </svg>
+);
+const TrashIcon = (props) => (
+  <svg
+    width={16}
+    height={16}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M14 3.98763C11.78 3.76763 9.54667 3.6543 7.32 3.6543C6 3.6543 4.68 3.72096 3.36 3.8543L2 3.98763"
+      stroke="#B6B8B9"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5.66602 3.31398L5.81268 2.44065C5.91935 1.80732 5.99935 1.33398 7.12602 1.33398H8.87268C9.99935 1.33398 10.086 1.83398 10.186 2.44732L10.3327 3.31398"
+      stroke="#B6B8B9"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M12.5669 6.09375L12.1336 12.8071C12.0603 13.8537 12.0003 14.6671 10.1403 14.6671H5.86026C4.00026 14.6671 3.94026 13.8537 3.86693 12.8071L3.43359 6.09375"
+      stroke="#B6B8B9"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6.88672 11H9.10672"
+      stroke="#B6B8B9"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M6.33398 8.33398H9.66732"
+      stroke="#B6B8B9"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+const HouseIcon = (props) => (
+  <svg
+    width={40}
+    height={40}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3.33398 36.666H36.6673"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4.91602 36.6668L4.99935 16.6167C4.99935 15.6001 5.48268 14.6335 6.28268 14.0002L17.9493 4.9168C19.1493 3.98346 20.8327 3.98346 22.0494 4.9168L33.716 13.9835C34.5327 14.6168 34.9994 15.5834 34.9994 16.6167V36.6668"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinejoin="round"
+    />
+    <path
+      d="M25.8327 18.334H14.166C12.7827 18.334 11.666 19.4507 11.666 20.834V36.6673H28.3327V20.834C28.3327 19.4507 27.216 18.334 25.8327 18.334Z"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M16.666 27.084V29.584"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M17.5 12.5H22.5"
+      stroke="#656568"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export {
+  AddPaymentMethodIcon,
+  CalendarIcon,
+  CardPOSIcon,
+  AwardIcon,
+  StarIcon,
+  TrashIcon,
+  HouseIcon,
+};
